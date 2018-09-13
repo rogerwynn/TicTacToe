1dcf3fa0dab3581c0f1f6afdeab80ba525b1210f
rogerwynn <43248328+rogerwynn@users.noreply.github.com>
2018-09-13 13:24:27 -0700

    Update tictactoe.java

--- src/com/tictactoe/tictactoe.java
+++ src/com/tictactoe/tictactoe.java
@@ -2,7 +2,7 @@ package com.tictactoe;
 
 // This class is equivelent to the "Main" class in generated template projects.
 // You left-click on the SRC folder after creating the project and give the package
-// name and class name you want for the main entry point (i.e. "com.tictactoe.tictactoegame").
+// name and class name you want for the main entry point (i.e. "com.tictactoe.tictactoe").
 public class tictactoe {
     public static void main(String[] args) {
         game mygame = new game();
